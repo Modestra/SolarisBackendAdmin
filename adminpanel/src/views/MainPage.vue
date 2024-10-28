1051380f518635a79e13991c106297e3808e1239
Карачев Кирилл <umbrellamixmail@gmail.com>
2024-10-29 01:26:19 +0700

    Подключение API

--- adminpanel/src/views/MainPage.vue
+++ adminpanel/src/views/MainPage.vue
@@ -1,9 +1,41 @@
 <template>
-    <main class="main-page">
-        Главная страница
-      </main>    
+  <main class="container main-page">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Имя</th>
+          <th scope="col">Фамилия</th>
+          <th scope="col">Обращение</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
+<style lang="css" scoped>
+.main-page {
+  min-height: 100vh;
+}
+</style>
