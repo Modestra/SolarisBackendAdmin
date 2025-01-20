20605a6c17358c034e2eb0d4334789ce0a1b0d62
Карачев Кирилл <umbrellamixmail@gmail.com>
2025-01-20 17:56:40 +0700

    fix

--- adminpanel/src/router/index.js
+++ adminpanel/src/router/index.js
@@ -65,7 +65,7 @@ router.beforeEach((to, from, next) => {
   const user = useUserStore();
 
   if (to.matched.some((route) => route.meta.protected)) {
-    if (token && user.admin) { //Не соответствую геттеры и сеттеры
+    if (token) {
       next();
     } else {
       next('/auth');
