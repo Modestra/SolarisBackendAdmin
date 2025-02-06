320d5173f4af28b5e0a310de70ef5e52a5c84cce
unknown <oleg21082001@mail.ru>
2025-02-06 14:42:19 +0300

    fix

--- adminpanel/src/features/competitions/pages/CompetitionsPage.vue
+++ adminpanel/src/features/competitions/pages/CompetitionsPage.vue
@@ -1,6 +1,35 @@
-<template></template>
+<template>
+  <div class="page">
+    <div>
+      <h1>Активность</h1>
+      <form class="flex gap-3">
+        <SelectButton
+          v-model="selectedRole"
+          :options="roles"
+          optionLabel="label"
+          optionValue="value"
+        />
+      </form>
+    </div>
+    <CreateCompetitionForm v-if="selectedRole === 'createCompetition'" />
+    <CompetitionsComponent v-if="selectedRole === 'checkCompetition'" />
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Roles } from '../types';
+import CompetitionsComponent from '../components/CompetitionsComponent.vue';
+import CreateCompetitionForm from '../../../components/form/CreateCompetitionForm.vue';
+import SelectButton from 'primevue/selectbutton';
+
+const roles: Roles[] = [
+  { value: 'createCompetition', label: 'Создать конкурс' },
+  { value: 'checkCompetition', label: 'Просмотреть конкурсы' },
+];
+
+const selectedRole = ref<string>('createCompetition');
+</script>
 
 <style lang="scss">
 .page {
--- adminpanel/src/stores/competitionStore.ts
+++ adminpanel/src/stores/competitionStore.ts
@@ -1,11 +1,11 @@
-import { defineStore } from "pinia";
-import type { CompetitionFields } from "../interfaces/competition/competitionInterface";
-import { getCompetitionService } from "../services/get/getCompetition";
-import { addCompetitionService } from "../services/add/addCompetition";
-import { deleteCompetitionService } from "../services/delete/deleteCompetition";
-import { changeCompetitionService } from "../services/change/changeCompetition";
+import { defineStore } from 'pinia';
+import type { CompetitionFields } from '../interfaces/competition/competitionInterface';
+import { addCompetitionService } from '../features/competitions/services/competitionsService';
+import { deleteCompetitionService } from '../features/competitions/services/competitionsService';
+import { changeCompetitionService } from '../features/competitions/services/competitionsService';
+import { getCompetitionService } from '../features/competitions/services/competitionsService';
 
-export const useCompetitionStore = defineStore("competitionStore", {
+export const useCompetitionStore = defineStore('competitionStore', {
   state: () => ({
     competition: [] as CompetitionFields[],
   }),
@@ -19,18 +19,18 @@ export const useCompetitionStore = defineStore("competitionStore", {
       try {
         const res = await getCompetitionService();
         this.competition = res.data;
-        console.log("Competitions fetched:", this.competition);
+        console.log('Competitions fetched:', this.competition);
       } catch (error) {
-        console.error("Error fetching competitions:", error);
+        console.error('Error fetching competitions:', error);
       }
     },
     async addCompetition(competition: CompetitionFields) {
       try {
         const res = await addCompetitionService(competition);
         this.competition.push(res.data);
-        console.log("Competition added:", res.data);
+        console.log('Competition added:', res.data);
       } catch (error) {
-        console.error("Error adding competition:", error);
+        console.error('Error adding competition:', error);
       }
     },
     async deleteCompetition(id: string) {
@@ -39,18 +39,18 @@ export const useCompetitionStore = defineStore("competitionStore", {
         this.competition = this.competition.filter((comp) => comp.id !== id);
         console.log(`Competition with ID ${id} deleted`);
       } catch (error) {
-        console.error("Error deleting competition:", error);
+        console.error('Error deleting competition:', error);
       }
     },
-    
+
     async changeCompetition(
       competition_id: string,
-      competition: CompetitionFields
+      competition: CompetitionFields,
     ) {
       try {
         await changeCompetitionService(competition_id, competition);
       } catch (error) {
-        console.error("Error deleting competition:", error);
+        console.error('Error deleting competition:', error);
       }
     },
   },
